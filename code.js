ef4ec165781baa2c91f50128cfefacf8921bd4e9
Santiago <santiago800g@gmail.com>
2025-11-21 11:17:23 -0600

    Add code to customize the number of squares

--- code.js
+++ code.js
@@ -1,14 +1,27 @@
-const container =  document.querySelector('.container');
-
-for(let i = 0; i < 16 ; i++){
-    let row = document.createElement('div');
-    row.style.display = 'flex';
-    row.style.flex = '1';
-    container.appendChild(row);
-    for(let j = 0 ; j < 16 ; j++){
-        let square = document.createElement('div');
-        square.style.border = '1px solid black';
-        square.style.flex = '1';
-        row.appendChild(square);
-    }
+function createGrid(gridDimensions) {
+    for(let i = 0; i < gridDimensions ; i++){
+        let row = document.createElement('div');
+        row.style.display = 'flex';
+        row.style.flex = '1';
+        container.appendChild(row);
+        for(let j = 0 ; j < gridDimensions ; j++){
+            let square = document.createElement('div');
+            square.style.border = '1px solid black';
+            square.style.flex = '1';
+            square.addEventListener('mouseenter', () => square.style.backgroundColor = 'black');
+            row.appendChild(square);
+        }
+    }    
 }
+
+const container =  document.querySelector('.container');
+const button = document.querySelector('button');
+let numberOfSquares;
+button.addEventListener('click', () => {
+    numberOfSquares = prompt('Number of squares: ');    
+    container.innerHTML = '';
+    createGrid(numberOfSquares);    
+});
+
+
+
